26fb898476d3cd7e7e52741b97badb1bdc5e66f2
Diogo Pereira <diogopereira5.ds@gmail.com>
2022-12-12 10:34:43 -0300

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomePage from "../pages/Home"
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="Home" component={HomePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
